7ebb96ad41cc4632d857eb47d47f22f638f362eb
Dmitry Abulkhanov <heapnhash@gmail.com>
2023-03-13 15:58:25 +0300

    Refactor some places

--- pii/ner_model_training/utils/chunking.py
+++ pii/ner_model_training/utils/chunking.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import List, Iterable, Any
+from typing import List, Iterable, Any, Optional
 
 import numpy as np
 from datasets import Dataset
@@ -35,11 +35,10 @@ def chunk_dataset(dataset: Dataset, tokenizer: PreTrainedTokenizer, overlap_freq
 
 def chunk_inputs(input_ids,
                  attention_mask,
-                 labels,
                  id,
-                 *,
                  tokenizer,
                  max_length,
+                 labels=None,
                  overlap_freq=0,
                  **kwargs):
     """
@@ -50,33 +49,41 @@ def chunk_inputs(input_ids,
 
     Returns
     -------
-    list of dictionaries that contain the chunked `input_ids`, `attention_mask`, `labels`, and `chunk_id`
+    list of dictionaries that contain the chunked `input_ids`, `attention_mask`, `labels`
 
     """
 
-    def _chunked_seq(seq: Iterable[Any], length: int, overlap_freq: int = 0):
-        step = _get_chunking_step(length, overlap_freq)
+    step = _get_chunking_step(max_length, overlap_freq)
 
+    def _chunked_seq(seq):
         for i in range(len(seq) // step + 1):
             if i * step < len(seq):
-                yield seq[i * step:i * step + length]
+                yield seq[i * step:i * step + max_length]
 
-    chunks = zip(*[_chunked_seq(seq, max_length, overlap_freq) for seq in (input_ids, attention_mask, labels)])
-    chunks = (_prepare_for_model(*chunk, tokenizer=tokenizer) for chunk in chunks)
+    if labels is not None:
+        chunks = zip(*[_chunked_seq(seq) for seq in (input_ids, attention_mask, labels)])
+        chunks = (_prepare_for_model(*chunk, tokenizer=tokenizer) for chunk in chunks)
+    else:
+        chunks = zip(*[_chunked_seq(seq) for seq in (input_ids, attention_mask)])
+        chunks = (_prepare_for_model(*chunk, labels=None, tokenizer=tokenizer) for chunk in chunks)
 
     return [dict(input_ids=input_ids,
                  attention_mask=attention_mask,
                  labels=labels,
+                 step=step,
                  id=id,
                  chunk_id=i)
             for i, (input_ids, attention_mask, labels) in enumerate(chunks)]
 
 
-def _prepare_for_model(input_ids: List[int], attention_mask: List[int], labels: List[int], *,
+def _prepare_for_model(input_ids: List[int], attention_mask: List[int], labels: Optional[List[int]] = None, *,
                        tokenizer: PreTrainedTokenizer, null_label_id: int = LABEL2ID['O']):
     input_ids = [tokenizer.cls_token_id] + list(input_ids) + [tokenizer.sep_token_id]
     attention_mask = [1] + list(attention_mask) + [1]
-    labels = [null_label_id] + list(labels) + [null_label_id]
+    if labels is not None:
+        labels = [null_label_id] + list(labels) + [null_label_id]
+    else:
+        labels = None
 
     return input_ids, attention_mask, labels
 
@@ -110,36 +117,29 @@ def _get_chunking_step(length: int, overlap_freq: int) -> int:
 def compose_chunk_predictions_with_samples(ref_dataset: Dataset, pred: PredictionOutput, entry_ids: List[int],
                                            tokenizer: PreTrainedTokenizer, overlap_freq: int = 2) -> Dataset:
     entry_ids = np.array(entry_ids)
-    raw_pred_probas = [collate_pred_chunks(pred.predictions[entry_ids == i], tokenizer, overlap_freq=overlap_freq)
+    raw_pred_probas = [collate_pred_chunks(pred.predictions[entry_ids == i],
+                                           tokenizer.max_len_single_sentence, overlap_freq=overlap_freq)
                        for i in sorted(set(entry_ids))]
-    raw_true_labels = [collate_label_chunks(pred.label_ids[entry_ids == i], tokenizer, overlap_freq=overlap_freq)
+    raw_true_labels = [collate_label_chunks(pred.label_ids[entry_ids == i],
+                                            tokenizer.max_len_single_sentence, overlap_freq=overlap_freq)
                        for i in sorted(set(entry_ids))]
 
     # Remove ignored index
     mask = np.array(raw_true_labels) != -100
     pred_labels = np.array(raw_pred_probas)[mask]
     true_labels = np.array(raw_true_labels)[mask]
-    #
-    # pred_labels = [
-    #     [p for (p, l) in zip(prediction, label) if l != -100]
-    #     for prediction, label in zip(raw_pred_probas, raw_true_labels)
-    # ]
-    # true_labels = [
-    #     [l for (p, l) in zip(prediction, label) if l != -100]
-    #     for prediction, label in zip(raw_pred_probas, raw_true_labels)
-    # ]
 
     ref_dataset = ref_dataset.map(lambda _, i: dict(labels=np.array(true_labels[i]), pred=np.array(pred_labels[i])),
                                   with_indices=True)
     return ref_dataset
 
 
-def collate_pred_chunks(pred_chunks: np.ndarray, tokenizer: PreTrainedTokenizer, overlap_freq: int = 2) -> np.ndarray:
+def collate_pred_chunks(pred_chunks: np.ndarray, max_length: int, overlap_freq: int = 2) -> np.ndarray:
     """
     Takes in a tensor of predicted probabilities from the model and aggregates them across chunks.
     It normalizes the probabilities using softmax along the example axis.
     """
-    step = _get_chunking_step(tokenizer.max_len_single_sentence, overlap_freq)
+    step = _get_chunking_step(max_length, overlap_freq)
     # drop special tokens
     pred_chunks = pred_chunks[:, 1:-1]
 
@@ -160,12 +160,11 @@ def collate_pred_chunks(pred_chunks: np.ndarray, tokenizer: PreTrainedTokenizer,
     return pred_proba
 
 
-def collate_label_chunks(label_id_chunks: np.ndarray, tokenizer: PreTrainedTokenizer,
-                         overlap_freq: int = 2) -> np.ndarray:
+def collate_label_chunks(label_id_chunks: np.ndarray, max_length: int, overlap_freq: int = 2) -> np.ndarray:
     """
     Takes in a tensor of true labels from the model and aggregates them across chunks.
     """
-    step = _get_chunking_step(tokenizer.max_len_single_sentence, overlap_freq)
+    step = _get_chunking_step(max_length, overlap_freq)
     # drop special tokens
     label_id_chunks = label_id_chunks[:, 1:-1]
 
